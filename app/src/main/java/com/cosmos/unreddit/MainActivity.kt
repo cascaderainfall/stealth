05d1ce2dd49e0ea655c5077ff872ef2745b7d0a6
CosmosDev <cosmos.dev@protonmail.com>
2021-03-23 21:40:34 -0400

    Update app name and branding

--- app/src/main/java/com/cosmos/unreddit/MainActivity.kt
+++ app/src/main/java/com/cosmos/unreddit/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private var currentNavController: LiveData<NavController>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
