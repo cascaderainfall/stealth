80260ddbf2ddaca58912d7301febe0792f9ed8c3
CosmosDev <cosmos.dev@protonmail.com>
2021-04-01 15:56:23 -0400

    Fix Reddit videos not playing when audio is not found

--- app/src/main/java/com/cosmos/unreddit/util/LinkUtil.kt
+++ app/src/main/java/com/cosmos/unreddit/util/LinkUtil.kt
@@ -12,6 +12,8 @@ object LinkUtil {
     private val SUBREDDIT_REGEX = Regex("/r/[A-Za-z0-9_-]{3,21}")
     private val USER_REGEX = Regex("/u/[A-Za-z0-9_-]{3,20}")
 
+    private const val REDDIT_SOUNDTRACK_NAME: String = "DASH_audio"
+
     fun getAlbumIdFromImgurLink(link: String): String {
         return HttpUrl.parse(link)?.pathSegments()?.getOrNull(1) ?: ""
     }
@@ -30,7 +32,11 @@ object LinkUtil {
     }
 
     fun getRedditSoundTrack(link: String): String {
-        return link.replace(REDDIT_VIDEO_REGEX, "DASH_audio")
+        return link.replace(REDDIT_VIDEO_REGEX, REDDIT_SOUNDTRACK_NAME)
+    }
+
+    fun isRedditSoundTrack(link: String): Boolean {
+        return link.contains(REDDIT_SOUNDTRACK_NAME)
     }
 
     fun getGfycatVideo(link: String): String {
