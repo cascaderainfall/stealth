2abdfb6f819ab07b23ac18a9d160f6323c4ea63d
CosmosDev <cosmos.dev@protonmail.com>
2022-04-18 18:06:41 +0200

    Add colors to profile avatars

--- app/src/main/java/com/cosmos/unreddit/ui/common/widget/AvatarView.kt
+++ app/src/main/java/com/cosmos/unreddit/ui/common/widget/AvatarView.kt
@@ -1,10 +1,14 @@
 package com.cosmos.unreddit.ui.common.widget
 
 import android.content.Context
+import android.content.res.ColorStateList
 import android.util.AttributeSet
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.cosmos.unreddit.R
+import com.cosmos.unreddit.util.extension.fitToRange
+import com.cosmos.unreddit.util.extension.sum
 import com.google.android.material.imageview.ShapeableImageView
 import java.util.Locale
 
@@ -18,6 +22,9 @@ class AvatarView @JvmOverloads constructor(
     private var initials: TextView
 
     private var text: String? = null
+
+    private val backgroundColor: Int
+        get() = text?.sum?.fitToRange(colorArray.indices)?.let { colorArray[it] } ?: colorArray[0]
 
     init {
         context.theme.obtainStyledAttributes(
@@ -40,16 +47,38 @@ class AvatarView @JvmOverloads constructor(
 
     override fun onFinishInflate() {
         super.onFinishInflate()
-        initials.text = textToInitials()
+        updateView()
     }
 
     fun setText(text: String?) {
         this.text = text
+        updateView()
+    }
+
+    private fun updateView() {
         initials.text = textToInitials()
+        avatar.backgroundTintList = ColorStateList.valueOf(
+            ContextCompat.getColor(context, backgroundColor)
+        )
     }
 
     private fun textToInitials(): String? {
         return text?.toUpperCase(Locale.getDefault())?.split("\\s")?.map { it.first() }
             ?.joinToString("")
+    }
+
+    companion object {
+        private val colorArray = arrayOf(
+            R.color.profile_background_1,
+            R.color.profile_background_2,
+            R.color.profile_background_3,
+            R.color.profile_background_4,
+            R.color.profile_background_5,
+            R.color.profile_background_6,
+            R.color.profile_background_7,
+            R.color.profile_background_8,
+            R.color.profile_background_9,
+            R.color.profile_background_10,
+        )
     }
 }
--- app/src/main/java/com/cosmos/unreddit/util/extension/NumberExt.kt
+++ app/src/main/java/com/cosmos/unreddit/util/extension/NumberExt.kt
@@ -3,6 +3,7 @@ package com.cosmos.unreddit.util.extension
 import android.content.Context
 import android.util.TypedValue
 import java.util.concurrent.TimeUnit
+import kotlin.math.abs
 
 fun Context.toPixels(value: Number, unit: Int = TypedValue.COMPLEX_UNIT_DIP): Float {
     return TypedValue.applyDimension(unit, value.toFloat(), resources.displayMetrics)
@@ -11,3 +12,7 @@ fun Context.toPixels(value: Number, unit: Int = TypedValue.COMPLEX_UNIT_DIP): Fl
 fun Long.toMillis(): Long {
     return TimeUnit.SECONDS.toMillis(this)
 }
+
+fun Int.fitToRange(range: IntRange): Int = abs(
+    this - ((this / (range.last - range.first)) * (range.last - range.first))
+)
