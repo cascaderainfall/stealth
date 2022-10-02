ecb37084d83a55a783d8203d54f2053ed70a3ff1
CosmosDev <cosmos.dev@protonmail.com>
2022-10-02 17:24:34 +0200

    Show toast when media download has started

--- app/src/main/java/com/cosmos/unreddit/ui/mediaviewer/MediaViewerFragment.kt
+++ app/src/main/java/com/cosmos/unreddit/ui/mediaviewer/MediaViewerFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
@@ -255,6 +256,12 @@ class MediaViewerFragment : FullscreenBottomSheetFragment() {
                 it.url,
                 it.type
             )
+
+            Toast.makeText(
+                requireContext(),
+                R.string.toast_download_started,
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 
